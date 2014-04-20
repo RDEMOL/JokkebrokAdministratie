70979e4c4e942864add0683d1b861c63d7e970d9
Floris Kint <floris.kint@gmail.com>
2014-04-21 00:10:15 +0200

    TODO update + nieuwe database structuur

--- config/database_structure.sql
+++ config/database_structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 19, 2014 at 08:58 PM
+-- Generation Time: Apr 21, 2014 at 12:09 AM
 -- Server version: 5.5.35-1ubuntu1
 -- PHP Version: 5.5.9-1ubuntu4
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `Aanwezigheid`
 --
 
-DROP TABLE IF EXISTS `Aanwezigheid`;
 CREATE TABLE IF NOT EXISTS `Aanwezigheid` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Datum` date NOT NULL,
@@ -44,7 +43,6 @@ CREATE TABLE IF NOT EXISTS `Aanwezigheid` (
 -- Table structure for table `Betaling`
 --
 
-DROP TABLE IF EXISTS `Betaling`;
 CREATE TABLE IF NOT EXISTS `Betaling` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `KindVoogd` int(11) NOT NULL,
@@ -60,7 +58,6 @@ CREATE TABLE IF NOT EXISTS `Betaling` (
 -- Table structure for table `Extraatje`
 --
 
-DROP TABLE IF EXISTS `Extraatje`;
 CREATE TABLE IF NOT EXISTS `Extraatje` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Omschrijving` text NOT NULL,
@@ -73,13 +70,12 @@ CREATE TABLE IF NOT EXISTS `Extraatje` (
 -- Table structure for table `ExtraatjeAanwezigheid`
 --
 
-DROP TABLE IF EXISTS `ExtraatjeAanwezigheid`;
 CREATE TABLE IF NOT EXISTS `ExtraatjeAanwezigheid` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Aanwezigheid` int(11) NOT NULL,
   `Extraatje` int(11) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=65 ;
 
 -- --------------------------------------------------------
 
@@ -87,7 +83,6 @@ CREATE TABLE IF NOT EXISTS `ExtraatjeAanwezigheid` (
 -- Table structure for table `Kind`
 --
 
-DROP TABLE IF EXISTS `Kind`;
 CREATE TABLE IF NOT EXISTS `Kind` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Voornaam` varchar(150) NOT NULL,
@@ -104,7 +99,6 @@ CREATE TABLE IF NOT EXISTS `Kind` (
 -- Table structure for table `KindVoogd`
 --
 
-DROP TABLE IF EXISTS `KindVoogd`;
 CREATE TABLE IF NOT EXISTS `KindVoogd` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Kind` int(11) NOT NULL,
@@ -120,14 +114,13 @@ CREATE TABLE IF NOT EXISTS `KindVoogd` (
 -- Table structure for table `Log`
 --
 
-DROP TABLE IF EXISTS `Log`;
 CREATE TABLE IF NOT EXISTS `Log` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Title` text NOT NULL,
   `Value` text NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=969 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -135,14 +128,14 @@ CREATE TABLE IF NOT EXISTS `Log` (
 -- Table structure for table `Uitstap`
 --
 
-DROP TABLE IF EXISTS `Uitstap`;
 CREATE TABLE IF NOT EXISTS `Uitstap` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Datum` date NOT NULL,
   `Omschrijving` text NOT NULL,
-  `Actief` tinyint(1) NOT NULL,
+  `DashboardZichtbaar` tinyint(1) NOT NULL,
+  `AanwezigheidZichtbaar` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -150,14 +143,13 @@ CREATE TABLE IF NOT EXISTS `Uitstap` (
 -- Table structure for table `UitstapKind`
 --
 
-DROP TABLE IF EXISTS `UitstapKind`;
 CREATE TABLE IF NOT EXISTS `UitstapKind` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Kind` int(11) NOT NULL,
   `Uitstap` int(11) NOT NULL,
   PRIMARY KEY (`Id`),
-  KEY `Kind_Uitstap` (`Kind`,`Uitstap`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+  UNIQUE KEY `Kind_Uitstap` (`Kind`,`Uitstap`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=46 ;
 
 -- --------------------------------------------------------
 
@@ -165,7 +157,6 @@ CREATE TABLE IF NOT EXISTS `UitstapKind` (
 -- Table structure for table `Voogd`
 --
 
-DROP TABLE IF EXISTS `Voogd`;
 CREATE TABLE IF NOT EXISTS `Voogd` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Naam` text NOT NULL,
@@ -180,7 +171,6 @@ CREATE TABLE IF NOT EXISTS `Voogd` (
 -- Table structure for table `Vordering`
 --
 
-DROP TABLE IF EXISTS `Vordering`;
 CREATE TABLE IF NOT EXISTS `Vordering` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Aanwezigheid` int(11) NOT NULL,
@@ -195,7 +185,6 @@ CREATE TABLE IF NOT EXISTS `Vordering` (
 -- Table structure for table `Werking`
 --
 
-DROP TABLE IF EXISTS `Werking`;
 CREATE TABLE IF NOT EXISTS `Werking` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Omschrijving` text NOT NULL,
