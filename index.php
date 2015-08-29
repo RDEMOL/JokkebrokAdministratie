6e6afc1d9e5de02bd4829a7b3f1b02d68a027416
Floris Kint <floris.kint@gmail.com>
2015-08-29 12:32:15 +0200

    Fix index.php issue when using htaccess

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 setlocale(LC_TIME, "nl_BE.UTF8");
-if (!$coming_from_public_html) {
-    header('Location: public_html/index.php');
-    exit;
-}
+//if (!$coming_from_public_html) {
+//    header('Location: public_html/index.php');
+//    exit;
+//}
 require_once(dirname(__FILE__) . "/model/model.php");
 require_once(dirname(__FILE__) . "/controller/controller.php");
 require_once(dirname(__FILE__) . "/view/view.php");
